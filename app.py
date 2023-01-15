a78e1ae8a37063b23147590d588cdd3f33367890
Sweetknock <JosephADodson@hotmail.com>
2023-01-14 22:50:37 -0800

    Includes weapon blessings with weapons

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ def html_table():
     #Load tables from dictionary
     df_dict = ProcessDataTables.get_last_update_as_dataframes()
     curios_df = df_dict["curios"]
+    
     weapon_df = df_dict["weapons"]
-    weapon_blessing_traits_df = df_dict["weapon-blessing-traits"]
-    modifiers_df = df_dict["modifiers"]
+    weapon_df["Weapon Blessings"] = [','.join([a +' - '+ b for (a,b) in zip(wb,wbd)]) for (wb, wbd) in zip(weapon_df["Weapon Blessings"].to_list(), weapon_df["Weapon Blessing Description"].to_list())]
+    weapon_df = weapon_df.drop("Weapon Blessing Description", axis=1)
+    #weapon_blessing_traits_df = df_dict["weapon-blessing-traits"]
+    #modifiers_df = df_dict["modifiers"]
 
-    return render_template('index.html', tables=[curios_df.to_html(classes='data'), weapon_df.to_html(classes='data'), weapon_blessing_traits_df.to_html(classes='data'), modifiers_df.to_html(classes='data')], 
-    titles=["Placeholder", "Curios", "Weapons", "Weapon Blessings and Traits", "Modifiers"])
+    return render_template('index.html', tables=[curios_df.to_html(classes='table-bordered', index=False), weapon_df.to_html(classes='table-bordered', index=False)], 
+    titles=["Placeholder", "Curios", "Weapons"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
